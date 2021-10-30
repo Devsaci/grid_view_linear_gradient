73f9f4688b2c7256f180e63a8ee3b4dde68a3039
saci zakaria <sacideveloppeur@gmail.com>
2021-10-30 22:27:11 +0200

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -35,45 +35,35 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("54. Grid View & Linear Gradient"),
       ),
-      body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text("He'd have you all unravel at the"),
-            color: Colors.teal[100],
+      body: Container(
+        padding: EdgeInsets.only(top: 15),
+        child: GridView(
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  color: Colors.orange
+              ),
+              child: Text("Container 1"),
+            ),
+            Container(
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  color: Colors.blue
+              ),
+              child: Text("Container 2"),
+            ),
+
+
+          ],
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 5/2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
           ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Heed not the rabble'),
-            color: Colors.teal[200],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Sound of screams but the'),
-            color: Colors.teal[300],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Who scream'),
-            color: Colors.teal[400],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Revolution is coming...'),
-            color: Colors.teal[500],
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('Revolution, they...'),
-            color: Colors.teal[600],
-          ),
-        ],
-      )
+        ),
+      ),
     );
   }
 }
